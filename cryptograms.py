2056c06506ec1fb9bb89f0a1604a9b3d0d12a229
Pink-Hat-Hacker <zyv@udel.edu>
2022-04-17 12:39:51 -0400

    made dictionary

--- cryptograms.py
+++ cryptograms.py
@@ -14,8 +14,24 @@ decrypted_str = []
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def main():
-    encrypt_input = input("Enter your encrypted text: ")
-    print(encrypt_input.split(" "))
+    # encrypt_input = input("Enter your encrypted text: ")
+    # encrypt_input = encrypt_input.split(" ")
+
+    #build the dictionary
+    make_dictionary()
+    for key, value in map_by_length.items():
+        print(key, " : ", value)
+
+#dictionary to search by length
+def make_dictionary():
+    for d in dictionary:
+        key = len(d)
+        if key in map_by_length:
+            map_by_length.get(key).append(d)
+        else:
+            new_list = []
+            new_list.append(d)
+            map_by_length[key] = new_list
 
 
 if __name__ == '__main__':
