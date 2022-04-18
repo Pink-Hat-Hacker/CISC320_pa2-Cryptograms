d046a33985be061c416e542d6de0c3df6760f463
Pink-Hat-Hacker <zyv@udel.edu>
2022-04-18 14:56:44 -0400

    this is dumb dont do this O(n^2)

--- cryptograms.py
+++ cryptograms.py
@@ -1,70 +1,14 @@
-from ctypes import pointer
 import sys
 
-#open file in dictionary
-filename = 'dictionary.txt'
-file_open = open(filename)
-dictionary = file_open.read().split('\n')
-file_open.close()
+encrypt_input = input("Enter your encrypted text: ")
 
-#map by length dictionary
-map_by_length = dict()
-#decrypted string list
-decrypted_str = []
-#alphabet
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-def main():
-    encrypt_input = input("Enter your encrypted text: ")
-    encrypt_input = encrypt_input.split(" ")
-    
-    #build the dictionary
-    make_dictionary()
-    #solve encrypted text
-    possible_solutions(encrypt_input)
-
-    # for key, value in map_by_length.items():
-    #     print(key, " : ", value)
-def update_map(decode, input_text, possible_text):
-    input_array = list(input_text)
-    possible_array = list(possible_text)
-    possible_encode = True
-
-    for i in input_array:
-        if ((decode.get(i) == "!") and (decode not in possible_array[i])):
-            decode.replace(i, possible_array[i])
-        elif ((decode.get(i) != possible_array[i])):
-            possible_encode = False
-            break
-    return possible_encode
-
-def decoding():
-    character_map = dict()
-    for c in alphabet:
-        character_map[c] = '!'
-    return character_map
-
-def solve_decrypt(text, decoded_str, decode, index):
-    pass
-
-def possible_solutions(text):
-    current_length_list = map_by_length.get(len(text[0]));
-    for t in current_length_list:
-        decode = decoding()
-        if(update_map(decode, text[0], t)):
-            solve_decrypt(text, t, decode, 1)
-
-#dictionary to search by length
-def make_dictionary():
-    for d in dictionary:
-        key = len(d)
-        if key in map_by_length:
-            map_by_length.get(key).append(d)
-        else:
-            new_list = []
-            new_list.append(d)
-            map_by_length[key] = new_list
-
-
-if __name__ == '__main__':
-    main()
+for i in range(26):
+    word = ''
+    for j in range(len(encrypt_input)):
+        extract = ord(encrypt_input[j]) - 97
+        decrypt_value = ((extract - i) % 26) + 97
+        word += chr(decrypt_value)
+    if w in word:
+        print("Decrypt Word is:", word)
+        print("The key is:", i)
+ 
