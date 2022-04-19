6e6e345d92f7e9d74f3b973519de49e915cf6a24
Pink-Hat-Hacker <zyv@udel.edu>
2022-04-19 11:55:07 -0400

    following silber ppt

--- cryptograms.py
+++ cryptograms.py
@@ -1,4 +1,3 @@
-from ctypes import pointer
 import sys
 
 #open file in dictionary
@@ -9,67 +8,22 @@ file_open.close()
 
 #map by length dictionary
 map_by_length = dict()
-#decrypted string list
-decrypted_str = []
 #alphabet
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 #input
-encrypt_input = ""
+#encrypt_input = ""
+#all solutions found?
+finished = False
 
 def main():
-    encrypt_input = input("Enter your encrypted text: ")
-    encrypt_input = encrypt_input.split(" ")
+    # encrypt_input = input("Enter your encrypted text: ")
+    # encrypt_input = encrypt_input.split(" ")
     
     #build the dictionary
     make_dictionary()
-    #solve encrypted text
-    possible_solutions(encrypt_input)
 
-    # for key, value in map_by_length.items():
-    #     print(key, " : ", value)
-def update_map(decode, input_text, possible_text):
-    input_array = list(input_text)
-    possible_array = list(possible_text)
-    possible_encode = True
-
-    for i in input_array:
-        if ((decode.get(i) == "!") and (decode not in possible_array[i])):
-            decode.replace(i, possible_array[i])
-        elif ((decode.get(i) != possible_array[i])):
-            possible_encode = False
-            break
-    return possible_encode
-
-def decoding():
-    character_map = dict()
-    for c in alphabet:
-        character_map[c] = '!'
-    return character_map
-
-def solve_decrypt(text, decoded_str, decode, index):
-    matching = False
-
-    if (len(encrypt_input) == len(decoded_str)):
-        decrypted_str.append(decoded_str)
-        matching = True
-    else:
-        same_length = map_by_length.get(len(text.get(index)))
-        for same in same_length:
-            new_decode = decoding()
-            new_decode.update(decode)
-
-            if(update_map(new_decode, text.get(index), same)):
-                update_decrypted_str = decrypted_str + " " + same
-                solve_decrypt(text, update_decrypted_str, new_decode, index+1)
-    return matching
-
-
-def possible_solutions(text):
-    current_length_list = map_by_length.get(len(text[0]));
-    for t in current_length_list:
-        decode = decoding()
-        if(update_map(decode, text[0], t)):
-            solve_decrypt(text, t, decode, 1)
+    for key, value in map_by_length.items():
+        print(key, " : ", value)
 
 #dictionary to search by length
 def make_dictionary():
@@ -82,6 +36,28 @@ def make_dictionary():
             new_list.append(d)
             map_by_length[key] = new_list
 
+def backtrack(a, k, input):
+    c = []
+    num_c = 0
+    counter = 0
+
+    if(is_solution(a, k, input)):
+        process_solutions(a, k, input)
+    else:
+        k += 1
+        construct_c(a, k, input, c, num_c)
+        for counter in num_c:
+            a[k] = c[counter]
+            backtrack(a, k, input)
+            if finished:
+                break
+
+def is_solution(a, k, input):
+    pass
+def construct_c(a, k, input, c, num_c):
+    pass
+def process_solutions(a, k):
+    pass
 
 if __name__ == '__main__':
     main()
